e39a3ab5ab23aa6abd57132abbb4008a69b86516
benlazzero <85390423+benlazzero@users.noreply.github.com>
2023-10-22 09:40:39 -0700

    init

---
+++ db-postgres/seedTemplate.sql
@@ -0,0 +1,2 @@
+INSERT INTO user_templates (owner_id, version, difference, template_id, name) VALUES (1, 1, '{"pageTitle":["Sample Page","tester"],"components":{"0":{"props":{"textContent":{"0":["Pizza"],"_t":"a","_0":["About",0,0]}}},"_t":"a"}}', 1, 'pizza');
+INSERT INTO users (name) VALUES ('tester');
